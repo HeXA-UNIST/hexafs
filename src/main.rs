f47ee7d2c7213e2e63554691f5187c95bf80b113
phynalle <phynalle@gogyeong-wan-ui-MacBook.local>
2016-09-22 01:11:44 +0900

    Add server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::env;
+
+mod server;
+
 fn main() {
-    println!("Hello, HeXA!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() >= 2 {
+        match &args[1] as &str {
+            "master" => {
+                server::master::run();
+                return;
+            }
+            "chunkserver" => {
+                server::chunkserver::run();
+                return;
+            }
+            _ => {}
+        }
+    }
+
+    print_usage();
+    return;
+
+}
+
+fn print_usage() {
+    let usage = "Usage:
+    hxfs master [arguments]
+    hxfs chunkserver [arguments]
+";
+    println!("{}", usage);
 }
